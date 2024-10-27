037340e8d4e5362df5cc5bef13a820b85599e924
Anthony Sayre <anthony_sayre@msn.com>
2024-10-27 15:01:06 -0600

    remove sast high sev vuln

--- dotNETGoofV2.Website/Pages/Error.cshtml.cs
+++ dotNETGoofV2.Website/Pages/Error.cshtml.cs
@@ -39,18 +39,3 @@ namespace dotNETGoofV2.Website.Pages
         }
     }
 }
-namespace dotNETGoofV2.Website.Pages
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add("#app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            await builder.Build().RunAsync();
-        }
-    }
-}
