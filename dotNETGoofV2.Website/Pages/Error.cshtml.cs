171b21d49952a73b9043e83f149da76088bcf31f
Anthony Sayre <anthony_sayre@msn.com>
2024-10-27 12:19:57 -0600

    add CS packages

--- dotNETGoofV2.Website/Pages/Error.cshtml.cs
+++ dotNETGoofV2.Website/Pages/Error.cshtml.cs
@@ -7,6 +7,16 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace dotNETGoofV2.Website.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
